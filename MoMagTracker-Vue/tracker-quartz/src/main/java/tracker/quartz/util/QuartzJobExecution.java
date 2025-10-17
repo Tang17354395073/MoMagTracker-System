91a45cf3b42463dd32650b515ce27ba8f31c7a28
tangjiawei <15948660043@163.com>
2025-10-17 09:33:28 +0800

    第一次提交，生成总体概括 README.md 文件，以及初始版前端 MoMagTracker-Vue 和后端 MoMagTracker-Vue3 代码

---
+++ MoMagTracker-Vue/tracker-quartz/src/main/java/tracker/quartz/util/QuartzJobExecution.java
@@ -0,0 +1,19 @@
+package tracker.quartz.util;
+
+import org.quartz.JobExecutionContext;
+import tracker.quartz.domain.SysJob;
+
+/**
+ * 定时任务处理（允许并发执行）
+ * 
+ * @author ruoyi
+ *
+ */
+public class QuartzJobExecution extends AbstractQuartzJob
+{
+    @Override
+    protected void doExecute(JobExecutionContext context, SysJob sysJob) throws Exception
+    {
+        JobInvokeUtil.invokeMethod(sysJob);
+    }
+}
